97f5281036fc218c9fefa1608a07438a00cc0f43
Xiaoya Wang <861366745@qq.com>
2024-05-09 11:11:55 +0800

    table driven 风格第一次测试

--- split/split_test.go
+++ split/split_test.go
@@ -20,3 +20,25 @@ func TestMoreSplit(t *testing.T) {
 		t.Errorf("excepted:%v, got:%v", want, got)
 	}
 }
+
+func TestGroupSplit(t *testing.T) {
+	type prepare struct {
+		s    string
+		sep  string
+		want []string
+	}
+
+	data := map[string]prepare{
+		"college":   {s: "csu-18-33", sep: "-", want: []string{"csu", "18", "33"}},
+		"threebody": {s: "叶文洁·罗辑·程心", sep: "·", want: []string{"叶文", "罗辑", "程心"}},
+		"seasons":   {s: "spring*summer*fall*winter", sep: "*", want: []string{"spring", "summer", "fall", "winter"}},
+		"marks":     {s: "↑↓↑↓↑↓↑", sep: "↓", want: []string{"↑", "↑", "↑", "↑"}},
+	}
+
+	for name, d := range data {
+		got := Split(d.s, d.sep)
+		if !reflect.DeepEqual(got, d.want) {
+			t.Errorf("name:%s, expected:%#v, got:%#v", name, d.want, got)
+		}
+	}
+}
